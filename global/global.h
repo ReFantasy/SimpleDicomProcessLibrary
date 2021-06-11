d4b0e5e3102a0e534190209eb27f372c1112dbf2
ReFantasy <309741309@qq.com>
2021-06-11 21:42:58 +0800

    delete win32

--- global/global.h
+++ global/global.h
@@ -18,56 +18,5 @@ int StringToInt(std::string str);
 double StringToDouble(std::string str);
 
 
-#ifdef _WIN32
-#include <windows.h>
-#include <gdiplus.h>
-#include <gdiplusheaders.h>
-#include <gdiplusbase.h>
-#include <gdiplusbitmap.h>
-#include <atlimage.h>
 
-extern void* g_bitmap;
-
-std::vector<char> CreateBitmap(int width, int height, size_t imgsize, void* pdata);
-
-
-// 窗口处理函数
-LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-
-//注册窗口类
-BOOL Register(LPSTR lpClassName, WNDPROC wndProc, HINSTANCE g_hInstance);
-
-//创建主窗口
-HWND CreateMain(LPSTR lpClassName, LPSTR lpWndName, HINSTANCE g_hInstance);
-
-//显示窗口
-void Display(HWND hWnd);
-
-
-//消息循环
-void Message();
-
-struct RAWIMAGE
-{
-	int width;
-	int height;
-	unsigned char* data;
-};
-
-class Window
-{
-public:
-	int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-		_In_opt_ HINSTANCE hPrevInstance = HINSTANCE{},
-		_In_ LPWSTR    lpCmdLine = LPWSTR{},
-		_In_ int       nCmdShow = 0);
-
-	HWND hWnd;
-	HINSTANCE hInstance;
-};
-
-
-
-
-#endif
 #endif//__GLOBAL_H__
--- test/main.cpp
+++ test/main.cpp
@@ -1,69 +1,18 @@
-﻿#include <iostream>
-#include <windows.h>
+﻿#include <QApplication>
+#include <QMainWindow>
+#include <iostream>
+#include <thread>
+#include <vector>
 #include "global.h"
 #include "core.h"
 
-#include "dcmtk/config/osconfig.h"
-#include "dcmtk/dcmimgle/dcmimage.h"
-#include "dcmtk/dcmdata/dcfilefo.h"
-#include "dcmtk/dcmdata/dcdeftag.h"
-#include "dcmtk/dcmdata/dcuid.h"
-#include "dcmtk/dcmjpeg/djdecode.h"
-#include "dcmtk/dcmdata/dcmetinf.h"
-#include "dcmtk/ofstd/ofstd.h"
-
-#include <thread>
-#include <vector>
-
 using namespace std;
 
-
-
-int main()
+int main(int argc, char* argv[])
 {
-	DicomSeries series;
-	//auto b = series.ReadDir("/Users/refantasy/Desktop/CT1001051/401_351");
-	auto b = series.ReadDir("C:/Users/ReFantasy/Desktop/a");
-	if (!b)
-	{
-		cout << "read fail" << endl;
-		return 0;
-	}
-	cout << "read files success: file count  " << series.GetTotalFrames() << endl;
+	QApplication a(argc, argv);
+	QMainWindow w;
 
-	series.SetWindow(-500, 10);
-
-	auto df = series.GetDicom(100);
-	auto data = df->GetOutputData(0);
-	WriteToPPM("test.ppm", df->GetWidth(), df->GetHeight(), data);
-
-
-	//////////////////////////////////////////////////////////////////////////
-
-	void* pdata = NULL;
-	auto imgsize = df->GetDicomImage()->createWindowsDIB(pdata, 512 * 512, 0, 8, 1, 0);
-	cout << imgsize << endl;
-
-	auto bmp = CreateBitmap(df->GetDicomImage()->getWidth(), df->GetDicomImage()->getHeight(), imgsize, pdata);
-	FILE* fp = fopen("1.bmp", "wb");
-	if (fp)
-	{
-		fwrite(bmp.data(), sizeof(char), bmp.size(), fp);
-		fclose(fp);
-	}
-
-	//////////////////////////////////////////////////////////////////////////
-
-	std::shared_ptr<Window> pwin = std::make_shared<Window>();
-	auto f = [](std::shared_ptr<Window> pwin) {
-		pwin->wWinMain(pwin->hInstance);
-	};
-
-	std::thread t(f, pwin);
-
-	char* name = "hello";
-	while (1)
-	{
-		SendMessage(pwin->hWnd, WM_PAINT, (WPARAM)(name), (LPARAM)(name));
-	}
+	w.show();
+	return a.exec();
 }
